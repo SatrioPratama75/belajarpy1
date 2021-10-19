b29ea99335450a38a4d267c5f832fcbc29bfc619
SatrioPratama75 <satrio7299@gmail.com>
2021-10-20 03:46:59 +0700

    memasukanbiodata

---
+++ biodata.py
@@ -0,0 +1,22 @@
+##Input Data##
+
+nama = "Satrio Pratama Wijaya."
+panggilan = "Satrio."
+nim ="312110223."
+lahir = "Cianjur."
+umur = "18"
+alamat = "Perum. Kota Serang Baru Blok C.69 No.14."
+telepon = "085784992784"
+
+##Output Data##
+
+print ("Assalammu'alaikum..")
+
+print ()
+
+print ("Let me introduce my self. My name is " +nama,"But you can call me " +panggilan,"My NIM is " +nim,"I was born in " + lahir,"and I am "+umur,"years old.","I am very glad if you want to invite my house in " +alamat,"So don't forget to call me before with the number " +telepon)
+
+print ()
+print ("Thank you.")
+
+##Output done##
